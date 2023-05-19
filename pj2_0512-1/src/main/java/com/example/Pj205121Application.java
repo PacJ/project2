434904ba77dbc56e17dea1f59000e18c5005d3d2
PacJ <pjw9267@gmail.com>
2023-05-19 13:32:26 +0900

    2023-05-19

---
+++ pj2_0512-1/src/main/java/com/example/Pj205121Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj205121Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pj205121Application.class, args);
+	}
+
+}
